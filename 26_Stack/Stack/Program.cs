de8999647d6e64c8a633a971188e3b8206fbf6f7
Junaid Khalid <57348907+Junaid20001@users.noreply.github.com>
2024-07-21 10:46:26 +0500

    Add files via upload

---
+++ 26_Stack/Stack/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackExample // Renamed namespace to avoid conflict
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack myStack = new Stack();
+            myStack.Push("Junaid");
+            myStack.Push(22);
+            myStack.Push(6.4);
+            myStack.Push(true);
+            myStack.Push("Khalid");
+            myStack.Push(null);    //null
+            myStack.Push("Khalid");    //repeat
+            myStack.Push("Maiz");
+
+            //Console.WriteLine(myStack.Peek());  //show only last element
+            //Console.WriteLine(myStack.Contains("Junaid")); //element is present show true
+
+            //Console.WriteLine(myStack.Count);  //total element
+
+            foreach (object item in myStack)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("---------");
+
+            myStack.Pop();  //remove last element that add in table
+            foreach (object item in myStack)
+            {
+                Console.WriteLine(item);
+            }
+
+            //myStack.Clear();  //remove all elements
+
+            Console.ReadLine();
+        }
+    }
+
+}
